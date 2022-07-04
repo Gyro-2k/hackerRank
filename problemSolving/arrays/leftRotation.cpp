8c167453114aaae0d0030fb5884a8dedb653118f
D Gourisankar Mohanty <dgourisankarmohanty@gmail.com>
2022-07-04 07:10:10 +0530

    leftRotation solution (only the solution function)

---
+++ problemSolving/arrays/leftRotation.cpp
@@ -0,0 +1,12 @@
+//rotate the array by d elements
+
+vector<int> rotateLeft(int d, vector<int> arr) {
+    vector<int> ans;
+    for(int z=d;z<arr.size();z++){
+        ans.push_back(arr[z]);
+    }
+    for(int z=0;z<d;z++){
+        ans.push_back(arr[z]);
+    }
+    return ans;
+}
