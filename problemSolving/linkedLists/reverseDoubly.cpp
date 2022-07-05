f38f35a23d16ed49fc39ead254939585f9a8c1cd
D Gourisankar Mohanty <dgourisankarmohanty@gmail.com>
2022-07-05 10:58:47 +0530

    reverse a doubly link list (easy)

---
+++ problemSolving/linkedLists/reverseDoubly.cpp
@@ -0,0 +1,21 @@
+//solution to revere a doubly link list problem
+
+DoublyLinkedListNode* reverse(DoublyLinkedListNode* head) {
+    DoublyLinkedListNode *past=NULL,*present = head, *future;
+    while(present!=NULL){
+        future=present->next;
+        present->next=past;
+        past=present;
+        present=future;
+    }
+    present=past;
+    DoublyLinkedListNode *newHead=past;
+    past=NULL;
+    while(present!=NULL){
+        future=present->prev;
+        present->prev=past;
+        past=present;
+        present=future;
+    }
+    return newHead;
+}
