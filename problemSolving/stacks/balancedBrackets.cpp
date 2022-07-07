c7025dad0037a412f47634798b0905822210cbe0
D Gourisankar Mohanty <dgourisankarmohanty@gmail.com>
2022-07-07 06:09:52 +0530

    Balanced Brackets (medium)

---
+++ problemSolving/stacks/balancedBrackets.cpp
@@ -0,0 +1,48 @@
+//check if a string has balanced brackets
+
+string isBalanced(string s) {
+    string ans="YES";
+    stack<char> stkk;
+    for(int z=0;z<s.size();z++){
+        if(s[z]=='{' || s[z]=='(' || s[z]=='['){
+            stkk.push(s[z]);
+        }
+        else if(stkk.empty()){
+            ans="NO";
+            break;
+        }
+        else if(s[z]=='}'){
+            if(stkk.top() == '{'){
+                stkk.pop();
+            }
+            else{
+                ans="NO";
+                break;
+            }
+        }
+        
+        else if(s[z]==')'){
+            if(stkk.top()=='('){
+                stkk.pop();
+            }
+            else{
+                ans="NO";
+                break;
+            }
+        }
+        else {
+            if(stkk.top()=='['){
+                stkk.pop();
+            }
+            else{
+                ans="NO";
+                break;
+            }
+        }
+    }
+    if(!stkk.empty()){
+        ans="NO";
+    }
+    // cout<<"endone"<<endl;
+    return ans;
+}
