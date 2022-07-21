a427561747f22c756722c104d10c70ef19ddb172
D Gourisankar Mohanty <dgourisankarmohanty@gmail.com>
2022-07-21 21:25:05 +0530

    Functions (easy)

---
+++ c++/functions.cpp
@@ -0,0 +1,12 @@
+int max_of_four(int a,int b,int c,int d){
+    if(a>b && a>c && a>d){
+        return a;
+    }
+    else if(b>c && b>d){
+        return b;
+    }
+    else if(c>d){
+        return c;
+    }
+    return d;
+}
